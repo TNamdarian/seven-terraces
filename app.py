e720c74405e1566c1c00c589a547bffd23f88ca7
TNamdarian <teemanamdarian@gmail.com>
2022-02-09 15:53:39 +0000

    Add log out functionality

--- app.py
+++ app.py
@@ -105,6 +105,15 @@ def profile(username):
     return redirect(url_for("login"))
 
 
+# --- LOG OUT FUNCTIONALITY --- #
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
