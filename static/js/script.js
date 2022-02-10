562a73f5401c0d172f55251236f372796d2af808
TNamdarian <teemanamdarian@gmail.com>
2022-02-10 16:46:55 +0000

    Add select initialisation to JS

--- static/js/script.js
+++ static/js/script.js
@@ -3,11 +3,17 @@
          edge: "right"
      });
      $('.datepicker').datepicker({
-        format: "dd mmmm, yyyy",
-        yearRange: 3, 
-        showClearBtn: true,
-        i18n: {
-            done: "Select"
-        }
-    });
+         format: "dd mmmm, yyyy",
+         yearRange: 3,
+         showClearBtn: true,
+         i18n: {
+             done: "Select"
+         }
+     });
+ });
+
+ document.addEventListener("DOMContentLoaded", function() {
+     // select initialization
+     let selects = document.querySelectorAll("select");
+     M.FormSelect.init(selects);
  });
