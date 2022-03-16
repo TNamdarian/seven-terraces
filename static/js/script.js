7542d0a6c03e15427f83dd43ad9bc6beae9095ed
TNamdarian <teemanamdarian@gmail.com>
2022-03-16 16:37:15 +0000

    Add view and bookmark btn to property cards

--- static/js/script.js
+++ static/js/script.js
@@ -130,7 +130,6 @@ function setNavigation() {
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-
 function previewFile() {
     var preview = document.querySelector(".img-details img");
     var file = document.querySelector("input[type=file]").files[0];
